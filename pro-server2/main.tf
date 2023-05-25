808f720c159d05bfbb70532c9f66f44851ec9426
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-25 18:22:43 +0530

    Update main.tf

--- pro-server2/main.tf
+++ pro-server2/main.tf
@@ -1,37 +1,6 @@
-resource "aws_instance" "pro-server2" {
-  ami                    = "ami-02eb7a4783e7e9317"
-  instance_type          = "t2.medium"
-  key_name               = "exampl"
-  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]
-
-  tags = {
-    Name = "pro-server2"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sleep 60",
-      "echo 'Instance ready'"
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("./exampl.pem")
-    host        = self.public_ip
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.pro-server2.public_ip} > inventory"
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i /dev/stdin <<EOF\n${yaml_content}\nEOF"
-  }
-
-  locals {
-    yaml_content = <<-EOT
+variable "yaml_content" {
+  type    = string
+  default = <<-EOT
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -69,6 +38,38 @@ spec:
       port: 8082
       targetPort: 8082
 EOT
+}
+
+resource "aws_instance" "pro-server2" {
+  ami                    = "ami-02eb7a4783e7e9317"
+  instance_type          = "t2.medium"
+  key_name               = "exampl"
+  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]
+
+  tags = {
+    Name = "pro-server2"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+      "echo 'Instance ready'"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./exampl.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.pro-server2.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i /dev/stdin <<EOF\n${var.yaml_content}\nEOF"
   }
 }
 
