1130c7a279b9ef838d1e401c165d5dddce811aee
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-25 16:05:00 +0530

    Update main.tf

--- pro-server2/main.tf
+++ pro-server2/main.tf
@@ -1,19 +1,33 @@
- resource "aws_instance" "prod-server2" {
-  ami           = "ami-02eb7a4783e7e9317" 
-  instance_type = "t2.medium" 
-  key_name = "exampl"
+
+resource "aws_instance" "pro-server2" {
+  ami                    = "ami-02eb7a4783e7e9317"
+  instance_type          = "t2.medium"
+  key_name               = "exampl"
   vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("./exampl.pem")
-    host     = self.public_ip
-  }
-  provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
-  }
+
   tags = {
-    Name = "prod-server2"
+    Name = "pro-server2"
   }
-    
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+      "echo 'Instance ready'"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./exampl.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.pro-server2.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/pro-server2/pro-servere2/deploy.yml"
+  }
 }
