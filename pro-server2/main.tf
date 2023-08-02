601d51149fa42a801586855d62691537ee257085
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-08-02 12:00:43 +0530

    Update main.tf

--- pro-server2/main.tf
+++ pro-server2/main.tf
@@ -1,1 +1,32 @@
+resource "aws_instance" "k8s-pro" {
+  ami                    = "ami-02eb7a4783e7e9317"
+  instance_type          = "t2.medium"
+  key_name               = "exampl"
+  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]
 
+  tags = {
+    Name = "k8s-pro"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+      "echo 'Instance ready'"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./exampl.pem")
+    host        = self.public_ip
+  }
+  
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.k8s-pro.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/k8s-pro/k8s-pro/ansible-playbook.yml"
+  }
+}
